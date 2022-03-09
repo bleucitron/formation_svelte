85f9b839a102d4648cfb5bd544bd64f76b455628
Romain Crestey <rcrestey@gmail.com>
2022-03-09 16:25:16 +0100

    jour 1

---
+++ project/src/Clock.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { onMount } from 'svelte';
+  let date = new Date();
+
+  onMount(() => {
+    let interval = setInterval(() => {
+      date = new Date();
+      console.log('Date', date);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  });
+</script>
+
+<div>{date}</div>
